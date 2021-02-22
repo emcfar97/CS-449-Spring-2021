5778c6ccef218b3bd875b972aab052b26cbcc504
--1stRayos <--emc1140@hotmail.com>
2021-02-21 20:04:37 -0600

    Separated grid from board & added rank and file

--- board.py
+++ board.py
@@ -1,7 +1,8 @@
-from PyQt5.QtWidgets import QGridLayout, QPushButton, QWidget
+from PyQt5.QtWidgets import QGridLayout, QHBoxLayout, QVBoxLayout, QPushButton, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 # Code for gameboard. Accepts n rows and returns n x n matrix
-# of tiles
+# of tiles with rank and file
 class Board(QWidget):
     'TBA'
 
@@ -12,7 +13,46 @@ class Board(QWidget):
         self.create_widgets(rings)
 
     def configure_gui(self):
+
+        self.layout = QGridLayout()
+        self.layout.setSpacing(0)
+        self.setLayout(self.layout)
+
+    def create_widgets(self, rings):
         
+        # create main components of board
+        self.grid = Grid(self, rings)
+        self.file = QHBoxLayout(), QHBoxLayout()
+        self.rank = QVBoxLayout(), QVBoxLayout()
+
+        # populate rank and file with appropriate literals
+        for i in range(rings):
+
+            self.rank[0].addWidget(QLabel(str(i + 1), self))
+            self.rank[1].addWidget(QLabel(str(i + 1), self))
+            self.file[0].addWidget(QLabel(chr(i + 97), self))
+            self.file[1].addWidget(QLabel(chr(i + 97), self))
+        
+        # add grid, rank, and file to board
+        self.layout.addWidget(self.grid, 1, 1)
+        self.layout.addLayout(self.file[0], 0, 1)
+        self.layout.addLayout(self.file[1], 2, 1)
+        self.layout.addLayout(self.rank[0], 1, 0)
+        self.layout.addLayout(self.rank[1], 1, 2)
+
+# Code for grid. Accepts n rows and returns n x n matrix
+# of tiles 
+class Grid(QWidget):
+    'TBA'
+
+    def __init__(self, parent, rings=3):
+        
+        super(Grid, self).__init__(parent)
+        self.configure_gui()
+        self.create_widgets(rings)
+
+    def configure_gui(self):
+
         self.layout = QGridLayout()
         self.layout.setSpacing(0)
         self.setLayout(self.layout)
@@ -22,7 +62,9 @@ class Board(QWidget):
         self.tiles = []
 
         for row in range(rings):
+
             for col in range(rings):
+                
                 tile = Tile(self, f'{row}-{col}')
                 self.layout.addWidget(tile, row, col)
                 self.tiles.append(tile)
