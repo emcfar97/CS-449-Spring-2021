81b4a00b8916364e39c2ba8ed47dd2137630a171
--1stRayos <--emc1140@hotmail.com>
2021-02-23 15:00:46 -0600

    Added class for piece & temp function for cwd

--- board.py
+++ board.py
@@ -1,5 +1,15 @@
 from PyQt5.QtWidgets import QGridLayout, QHBoxLayout, QVBoxLayout, QPushButton, QWidget, QLabel
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+
+def set_directory():
+    '''Temporary function far changing current working directory
+    so that relative paths are correctly resolved'''
+
+    import os, pathlib
+
+    path = pathlib.Path(__file__).parent
+    os.chdir(path)
 
 # Code for gameboard. Accepts n rows and returns n x n matrix
 # of tiles with rank and file
@@ -63,6 +73,8 @@ class Grid(QWidget):
 
     def configure_gui(self):
 
+        self.setAcceptDrops(True)
+
         self.layout = QGridLayout()
         self.layout.setSpacing(50)
         self.setLayout(self.layout)
@@ -79,6 +91,10 @@ class Grid(QWidget):
                 self.layout.addWidget(tile, row, col)
                 self.tiles.append(tile)
 
+    def dropEvent(self, event):
+
+        self.addItem(event.mimeData().text())
+
 # Tile for gameboard. Currently pushbuttons, but would like
 # to make them generic widgets in future
 class Tile(QPushButton):
@@ -87,12 +103,30 @@ class Tile(QPushButton):
     def __init__(self, parent, name):
         
         super(Tile, self).__init__(name, parent)
-        self.configure_gui()
-    
-    def configure_gui(self):
-        
         self.setFixedSize(25, 25)
-    
+
+# Code for game pieces. Can be white or black based on type_
+# variable. Currently not capable of being dragged
+class Piece(QLabel):
+    'TBA'
+
+    def __init__(self, parent, type_):
+
+        super(Piece, self).__init__(parent)
+
+        if type_ == 0: path = r'Resources\black_piece.png'
+        elif type_ == 1: path = r'Resources\white_piece.png'
+        else: raise ValueError
+        
+        pixmap = QPixmap(path)
+        self.setPixmap(pixmap)
+
+    def dragMoveEvent(self, event):
+
+        return super().dragMoveEvent(a0)
+
+set_directory()
+
 # for running as a single file during debugging
 if __name__ == '__main__':
 
