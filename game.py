d69dd99c79dccce42b477cbc06445756b6a82dab
Rayos1 <emc1140@hotmail.com>
2021-03-10 11:42:15 -0600

    Preliminary code for main window

---
+++ game.py
@@ -0,0 +1,16 @@
+import board, logic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class Game(QMainWindow):
+
+    def __init__(self): pass
+
+    def configure_gui(self): pass
+
+    def create_widgets(self): pass
+    
+    def keyPressEvent(self): pass
+
+Qapp = QApplication([])
+
+Qapp.exec_()
