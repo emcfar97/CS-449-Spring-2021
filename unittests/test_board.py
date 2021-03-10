19910c88f26ddaf7e6ffe4910d2b8299db72efb9
Rayos1 <emc1140@hotmail.com>
2021-03-10 17:23:49 -0600

    Added testing file

---
+++ unittests/test_board.py
@@ -0,0 +1,38 @@
+import unittest
+
+from .. import board
+
+
+class TestSum(unittest.TestCase):
+
+    def test_____(self):
+        """
+        TBA
+        """
+        
+        self.assert____()
+
+    def test_____(self):
+        """
+        TBA
+        """
+        
+        self.assert____()
+
+    def test_____(self):
+        """
+        TBA
+        """
+        
+        self.assert____()
+
+    def test_____(self):
+        """
+        TBA
+        """
+        
+        self.assert____()
+
+if __name__ == '__main__':
+
+    unittest.main()
