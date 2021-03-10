4827f4555e3c78af5583a8ab0349969c8d9fe0e4
Rayos1 <emc1140@hotmail.com>
2021-03-10 13:15:12 -0600

    Filling in necessary code

--- game.py
+++ game.py
@@ -1,16 +1,36 @@
 import board, logic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtCore import Qt
 
 class Game(QMainWindow):
 
-    def __init__(self): pass
+    def __init__(self):
+        
+        super(Game, self).__init__()
+        self.setWindowTitle("Nine Men's Morris")
+        self.configure_gui()
+        self.create_widgets()
+        self.showMaximized()
 
-    def configure_gui(self): pass
+    def configure_gui(self):  
+        
+        self.center = QWidget(self)
+        self.layout = QHBoxLayout()
 
+        self.center.setLayout(self.layout)
+        self.setCentralWidget(self.center)
+        
     def create_widgets(self): pass
-    
-    def keyPressEvent(self): pass
+
+        # self.board = board.Board(self)
+
+    def keyPressEvent(self, event): 
+        
+        key_press = event.key()
+        if key_press == Qt.Key_Escape: self.close()
 
 Qapp = QApplication([])
+
+app = Game()
 
 Qapp.exec_()
