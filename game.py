65e1fd1c643cf3470c0bb4841774d75281d03a8e
Rayos1 <emc1140@hotmail.com>
2021-05-05 00:03:23 -0500

    Added menubar

--- game.py
+++ game.py
@@ -1,11 +1,7 @@
 import random, board, computer
-from PyQt5.QtWidgets import  QApplication, QMainWindow, QMessageBox, QWidget, QStatusBar, QHBoxLayout, QVBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import  QApplication, QMainWindow, QMessageBox, QWidget, QStatusBar, QHBoxLayout, QVBoxLayout, QSizePolicy, QMenuBar, QLabel, QCheckBox, QPushButton
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtCore import Qt
-import sys
-
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
 
 class Game(QMainWindow):
     """
@@ -15,24 +11,12 @@ class Game(QMainWindow):
         
         super(Game, self).__init__()
         self.setWindowTitle("Nine Men's Morris")
-        self.turn = random.randint(0, 1)
-        self.phase = 0
         self.configure_gui()
+        self.create_menu()
         self.create_widgets()
         self.showMaximized()
-
-        file1 = open('rules.txt', "r+")
-        mbox = QMessageBox.information(self, "RULES!!!!", file1.read())
-        file1.close()
         
-        first = {
-            0: 'Black', 1: 'White'
-            }
-        QMessageBox.information(
-            self, '', 
-            f'{first[self.turn]} goes first',
-            QMessageBox.Ok
-            )
+        self.begin()
 
     def configure_gui(self):
         
@@ -41,6 +25,15 @@ class Game(QMainWindow):
 
         self.center.setLayout(self.layout)
         self.setCentralWidget(self.center)
+    
+    def create_menu(self):
+
+        self.menubar = self.menuBar()
+        
+        self.menubar.addAction('Controls')
+        self.menubar.addAction('New Game', self.begin)
+        self.menubar.addAction('Quit Game', self.end)
+        self.menubar.addAction('Help', self.display_rules)
         
     def create_widgets(self):
 
@@ -53,34 +46,51 @@ class Game(QMainWindow):
         self.statusbar = QStatusBar(self)
         self.statusbar.setFixedHeight(30)
         self.setStatusBar(self.statusbar)
-        self.update_status()
-
-    def update_status(self):
-        """
-        Updates game statusbar for current pieces on 
-        board.
-        ### Assumes certain things, particularly in
-        ### phase 1, that may not be true in all 
-        ### situations
-        """
         
-        if   self.phase == 0:
-            # Currently assumes player taking opponent 
-            # pieces is not a thing.
+    def begin(self):
+        
+        self.turn = random.randint(0, 1)
+        self.phase = 0
 
-            black, white = self.board.piece_count()
-
-        elif self.phase == 1:
-            
-            black, white = self.board.piece_count()
-
-        elif self.phase == 2:
-            
-            black, white = self.board.piece_count()
-
-        self.statusbar.showMessage(
-            f'\tBlack Pieces: {black} \tWhite Pieces: {white}'
+        first = {
+            0: 'Black', 1: 'White'
+            }
+        QMessageBox.information(
+            self, '', 
+            f'{first[self.turn]} goes first',
+            QMessageBox.Ok
             )
+
+        self.update_status()
+        return
+
+        if self.WPlayerP.isChecked() and self.BPlayerP.isChecked():
+            mbox = QMessageBox.information(
+                self, "Players", "Two Player's are present, let the battle commence!!!!"
+                )
+        elif self.WPlayerP.isChecked():
+            mbox = QMessageBox.information(
+                self,  "Players",  "White has chosen to face our warrior, may the odds be in your favor!!!!"
+                )
+        elif self.BPlayerP.isChecked():
+            mbox = QMessageBox.information(
+                self,  "Players", "Black has chosen to face our warrior, may the odds be in your favor!!!!"
+                )
+        else:
+            mbox = QMessageBox.information(
+                self,  "Players", "No player has been chosen new game will not start"
+                )
+
+    def end(self):
+
+        mbox = QMessageBox.question(
+            self, "Warning!!!", "Are you sure you want to quit?",
+            QMessageBox.Yes | QMessageBox.No
+            )
+        if mbox == QMessageBox.Yes:
+          self.close()
+        elif mbox == QMessageBox.No:
+            mbox = QMessageBox.information(self, "Player","We are glad you have chosen to stay!!!!")
 
     def complete_turn(self):
         """
@@ -109,6 +119,39 @@ class Game(QMainWindow):
 
         self.update_status()
         self.turn += 1
+    
+    def update_status(self):
+        """
+        Updates game statusbar for current pieces on 
+        board.
+        ### Assumes certain things, particularly in
+        ### phase 1, that may not be true in all 
+        ### situations
+        """
+        
+        if   self.phase == 0:
+            # Currently assumes player taking opponent 
+            # pieces is not a thing.
+
+            black, white = self.board.piece_count()
+
+        elif self.phase == 1:
+            
+            black, white = self.board.piece_count()
+
+        elif self.phase == 2:
+            
+            black, white = self.board.piece_count()
+
+        self.statusbar.showMessage(
+            f'\tBlack Pieces: {black} \tWhite Pieces: {white}'
+            )
+
+    def display_rules(self):
+
+        QMessageBox.information(
+            self, "Player", open('rules.txt').read()
+            )
 
     def keyPressEvent(self, event):
         
@@ -125,22 +168,45 @@ class Controls(QWidget):
 
         super(Controls, self).__init__(parent)
         self.configure_gui()
-        # self.create_widgets()
+        self.create_widgets()
 
     def configure_gui(self):
 
-        layout = QVBoxLayout()
+        self.menubar = QMenuBar()
+
+        self.layout = QVBoxLayout()
         self.setSizePolicy(
             QSizePolicy.Expanding, QSizePolicy.Expanding
             )
 
+    def create_widgets(self):
+
+        # self.ribbon = QWidget(self)
+        # self.stats = QWidget(self)
+
+        # self.ribbon.setSizePolicy(
+        #     QSizePolicy.Expanding, QSizePolicy.Expanding
+        #     )
+        # self.stats.setSizePolicy(
+        #     QSizePolicy.Expanding, QSizePolicy.Expanding
+        #     )
+
+        # self.ribbon.setStyleSheet('background: red')
+        # self.stats.setStyleSheet('background: green')
+
+        # self.layout.addWidget(self.ribbon)
+        # self.layout.addWidget(self.stats)
+        
+        file = self.menubar.addMenu('File')
+
+        return
         #code for black player options, no funtions yet(in progress)
         BPlayer = QLabel("Black Player",self)
         BPlayer.setFont(font)
         BPlayer.move(700,200)
         self.BPlayerP = QCheckBox("Player", self)
         self.BPlayerP.move(700, 240)
-        BPlayer= QLabel(self)
+        BPlayer = QLabel(self)
         BPlayer.setPixmap(QPixmap('Resources/black_piece.png'))
         BPlayer.move(600, 220)
 
@@ -157,53 +223,26 @@ class Controls(QWidget):
         #Game control buttons, no functions (in progress)
         GControl = QLabel("Game controls", self)
         GControl.setFont(small_font)
-        GControl.move(700,500)
+        GControl.move(700, 500)
+
         startG = QPushButton("Start New Game",self)
         startG.clicked.connect(self.begin)
-        startG.move(700,540)
-        quitG=   QPushButton("Quit Game", self)
-        quitG.move(700,580)
+        startG.move(700, 540)
+        
+        quitG = QPushButton("Quit Game", self)
+        quitG.move(700, 580)
         quitG.clicked.connect(self.end)
+        
         Rules = QPushButton("Display rules",self)
-        Rules.move(700,620)
+        Rules.move(700, 620)
         Rules.clicked.connect(self.display_rules)
 
-    def begin(self):
-        if (self.WPlayerP.isChecked() and self.BPlayerP.isChecked()):
-            mbox= QMessageBox.information(self,"Players","Two Player's are present, let the battle commence!!!!")
-        elif(self.WPlayerP.isChecked()):
-            mbox = QMessageBox.information(self, "Players", "White has chosen to face our warrior, may the odds be in your favor!!!!")
-        elif(self.BPlayerP.isChecked()):
-            mbox = QMessageBox.information(self, "Players","Black has chosen to face our warrior, may the odds be in your favor!!!!")
-        else:
-            mbox = QMessageBox.information(self, "Players","No player has been chosen new game will not start")
-
-    def end(self):
-
-        mbox = QMessageBox.question(self,"Warning!!!", "Are you sure you want to quit?",QMessageBox.Yes | QMessageBox.No )
-        if mbox == QMessageBox.Yes:
-          sys.exit()
-        elif mbox == QMessageBox.No:
-            mbox = QMessageBox.information(self, "Player","We are glad you have chosen to stay!!!!")
-
     def display_rules(self):
+
         file1 = open('rules.txt', "r+")
         mbox = QMessageBox.information(self, "Player", file1.read())
         file1.close()
 
-    def create_widgets(self): 
-        
-        self.ribbon = QWidget(self)
-        self.stats = QWidget(self)
-        self.ribbon.setStyleSheet(
-            'background: red'
-            )
-        self.stats.setStyleSheet(
-            'background: blue'
-            )
-
-        self.layout.addWidget(self.stats)
-        
 Qapp = QApplication([])
 
 app = Game()
